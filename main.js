7d0e14d7bd60ac12400526a5a24624a649eef194
laifeipeng <1020103647@qq.com>
2019-03-06 12:08:24 +0800

    把爬取的照片存放到本地

--- main.js
+++ main.js
@@ -2,17 +2,48 @@
  * @Author: laifeipeng 
  * @Date: 2019-03-01 23:02:00 
  * @Last Modified by: laifeipeng
- * @Last Modified time: 2019-03-01 23:52:02
+ * @Last Modified time: 2019-03-06 11:18:25
  */
-
+const path = require('path');
+const fs = require('fs');
 const superagent = require('superagent');  //nodejs里一个非常方便的客户端请求代理模块
 const cheerio = require('cheerio'); //Node.js 版的jQuery
 const charset = require("superagent-charset"); //解决superagent中文乱码问题
 charset(superagent); //设置字符
+superagent.buffer['jpg'] = true;
+
+const resolve = dir => path.join('./', dir);
+console.log(resolve('a.html'))
 
 const BASE_URL = 'https://www.zbjuran.com/'; // 高清美女图片大全
 const SUB_PATH = 'mei/';
 const fullUrl = BASE_URL + SUB_PATH;
+
+//创建目录
+function mkdir(_path, callback) {
+  if (fs.existsSync(_path)) {
+    console.log(`${_path}目录已存在`)
+  } else {
+    fs.mkdir(_path, (error) => {
+      if (error) {
+        return console.log(`创建${_path}目录失败`);
+      }
+      console.log(`创建${_path}目录成功`)
+    })
+  }
+  callback(); //没有生成指定目录不会执行
+}
+
+//下载爬到的图片
+function downloadImg() {
+  rst.forEach((imgUrl, index) => {    
+    //下载图片存放到指定目录
+    const stream = fs.createWriteStream(`./pic/${imgUrl.alt}.jpg`);
+    const req = superagent.get(imgUrl.picUrl); //响应流
+    req.pipe(stream);
+    console.log(`开始下载图片 https:${imgUrl.picUrl} --> ./pic/${imgUrl.picUrl}`);
+  })
+}
 
 const rst = []; // 存放最后的结果
 
@@ -26,7 +57,7 @@ superagent.get(fullUrl).charset('gbk')
     $('.main .pic-list li .picbox img').each((idx, element) => {
       const $element = $(element)['0']['attribs'];
       let pic = $element['data-original'];
-      !$element['data-original'].includes(BASE_URL) && (pic = BASE_URL + pic); // 如果没有前缀则加上
+      !$element['data-original'].includes('//www.zbjuran.com/') && (pic = 'https://www.zbjuran.com/' + pic); // 如果没有前缀则加上
 
       rst.push({
         picUrl: pic,
@@ -36,4 +67,5 @@ superagent.get(fullUrl).charset('gbk')
       })
     })
     console.log(rst);
+    mkdir('./pic', downloadImg);
   })
